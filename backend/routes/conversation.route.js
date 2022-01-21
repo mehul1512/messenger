f577ea734fd76047a07e92d7d232f1588a6e8da5
DISHEN MAKWANA <dishenmakwana.dm@gmail.com>
2022-01-21 18:22:27 +0530

    remove authMiddlewares for api testing in frontend
    Signed-off-by: DISHEN MAKWANA <dishenmakwana.dm@gmail.com>

--- backend/routes/conversation.route.js
+++ backend/routes/conversation.route.js
@@ -9,12 +9,12 @@ const { authMiddleware } = require('../middleware/auth.middleware');
 const router = express.Router();
 
 //new conversation
-router.post('/', authMiddleware, newConversation);
+router.post('/', newConversation); // authMiddleware,
 
 //get conversation of user
-router.get('/:user_id', authMiddleware, getConversationOfUser);
+router.get('/:user_id', getConversationOfUser); // authMiddleware,
 
 // get conversation includes two users
-router.get('/:user_id1/:user_id2', authMiddleware, getConversationOfTwoUsers);
+router.get('/:user_id1/:user_id2', getConversationOfTwoUsers); // authMiddleware,
 
 module.exports = router;
