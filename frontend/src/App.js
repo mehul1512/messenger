1f6879cbecd432247ee4fc9abd2d75f4339f957d
DISHEN MAKWANA <dishenmakwana.dm@gmail.com>
2022-01-21 22:24:27 +0530

    dynamic data fetch from backend api
    Signed-off-by: DISHEN MAKWANA <dishenmakwana.dm@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,20 @@
-import './App.css';
-import Messenger from './pages/messengers/messenger';
-import Login from './pages/login/login';
-import Register from './pages/register/register';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import Messenger from './pages/messenger/Messenger';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  
-  return (
-    <>
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/messenger" exact component={Messenger} />
-        <Route path="/register" exact component={Register} />
-      </Switch>
-    </Router>
-          
-      
-      
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route path='/' exact component={Login} />
+                    <Route path='/messenger' exact component={Messenger} />
+                    <Route path='/register' exact component={Register} />
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
